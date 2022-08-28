98731794332d009feacc239e058c0eb4bd80d458
alphy1 <alphiya01@mail.ru>
2022-08-28 19:58:51 +0300

    Save items to `mongodb`

--- difc_parser/pipelines.py
+++ difc_parser/pipelines.py
@@ -6,17 +6,30 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-import json
+import pymongo
 
 
 class DifcParserPipeline:
+    collection_name = 'difc_items'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+        )
+
     def open_spider(self, spider):
-        self.file = open('items.jl', 'w')
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
 
     def close_spider(self, spider):
-        self.file.close()
+        self.client.close()
 
     def process_item(self, item, spider):
-        line = json.dumps(ItemAdapter(item).asdict()) + "\n"
-        self.file.write(line)
+        self.db[self.collection_name].insert_one(ItemAdapter(item).asdict())
         return item
